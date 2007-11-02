f2ad8d74dcd00fb967ae31fd8d8a4a73b512c032
schneidh <schneidh@325f7265-e43d-0410-85fd-bdcf64f8f5e9>
2007-11-02 03:57:57 +0000

    really added begin/end annotations
    git-svn-id: http://struts2scopeplugin.googlecode.com/svn/trunk@8 325f7265-e43d-0410-85fd-bdcf64f8f5e9

---
+++ plugin/src/main/java/com/googlecode/scopeplugin/annotations/End.java
@@ -0,0 +1,35 @@
+package com.googlecode.scopeplugin.annotations;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Marks a method as ending a conversation, if the
+ * method returns a non-null outcome without throwing 
+ * an exception. If a list of outcomes is specified, 
+ * the conversation ends only if the outcome is in 
+ * the list. A null outcome never ends the conversation.
+ * If the method is of type void, the conversation always
+ * ends.
+ * 
+ * @author Gavin King
+ */
+@Target(METHOD)
+@Retention(RUNTIME)
+@Documented
+public @interface End 
+{
+   /**
+    * Should the conversation be destroyed before any
+    * redirect? (The default behavior is to propagate
+    * the conversation across the redirect and then
+    * destroy it at the end of the redirected request.)
+    * 
+    * @return false by default
+    */
+   boolean beforeRedirect() default false;
+}
